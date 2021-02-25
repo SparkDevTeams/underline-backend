b8f6fa0397b8019b5b97bc7a75e3cc721e372720
Christian Valencia <cvale069@fiu.edu>
2021-02-24 19:00:13 -0500

    Added 3 user password's unit tests

--- tests/test_user_login_unit.py
+++ tests/test_user_login_unit.py
@@ -14,3 +14,19 @@ class TestChangePasswordUnit:
         unregistered_user.set_password(new_pass)
         assert check_password_changed(unregistered_user, new_pass)
 
+    def test_change_wrong_password(self, unregistered_user):
+        """
+        Checks if a wrong password is accepted
+        """
+        new_pass = "new password"
+        unregistered_user.set_password(new_pass)
+        random_str = "random string"
+        assert not check_password_changed(unregistered_user, random_str)
+
+    def test_change_empty_password(self, unregistered_user):
+        """
+        Checks if an empty password was changed correctly
+        """
+        new_pass = ""
+        unregistered_user.set_password(new_pass)
+        assert check_password_changed(unregistered_user, new_pass)
