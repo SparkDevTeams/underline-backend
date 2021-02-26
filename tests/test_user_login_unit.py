4ef87f6dd4def1d052ab6da7135a923b0cc6940c
Christian Valencia <cvale069@fiu.edu>
2021-02-26 11:38:48 -0500

    Fixed lint errors

--- tests/test_user_login_unit.py
+++ tests/test_user_login_unit.py
@@ -1,12 +1,19 @@
+# pylint: disable=no-self-use
+#       - pylint test classes must pass self, even if unused.
 """
 Unit tests for user authentication utility
 """
 
-def check_password_changed(user, new_pass) -> bool:
+import models.users as user_models
+
+def check_password_changed(user: user_models.User, new_pass: str) -> bool:
+    """
+    Checks if a String new_pass is the same as the user's password
+    """
     return user.check_password(new_pass)
 
 class TestChangePasswordUnit:
-    def test_change_password_success(self, unregistered_user):
+    def test_change_password_success(self, unregistered_user: user_models.User):
         """
         Checks if password was changed correctly
         """
@@ -14,7 +21,7 @@ class TestChangePasswordUnit:
         unregistered_user.set_password(new_pass)
         assert check_password_changed(unregistered_user, new_pass)
 
-    def test_change_wrong_password(self, unregistered_user):
+    def test_change_wrong_password(self, unregistered_user: user_models.User):
         """
         Checks if a wrong password is accepted
         """
@@ -23,7 +30,7 @@ class TestChangePasswordUnit:
         random_str = "random string"
         assert not check_password_changed(unregistered_user, random_str)
 
-    def test_change_empty_password(self, unregistered_user):
+    def test_change_empty_password(self, unregistered_user: user_models.User):
         """
         Checks if an empty password was changed correctly
         """
