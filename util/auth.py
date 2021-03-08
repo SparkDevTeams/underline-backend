392841c3772db7ef8bb4bd6f718d209aca51a7a9
Jason Davila <jsndavila1@gmail.com>
2021-03-08 14:42:48 -0500

    added python-jose to requirements, added token authentication from header.

--- util/auth.py
+++ util/auth.py
@@ -1,8 +1,14 @@
 """
 Has some helper/util functions for general authentication handling.
 """
-from typing import Dict, Any
-from fastapi import Header
+
+import json
+import datetime
+
+from typing import Dict, List, Optional, Any
+from fastapi import Header, HTTPException
+from jose import jwt
+
 import models.auth as token_model
 from models import exceptions
 
@@ -13,9 +19,23 @@ async def get_auth_token_from_header(token: str = Header(None)) -> str:
     Attempts to get the auth token string from the request header,
     returning it if available, else raises an authentication error.
     """
-    if not token:
-        raise exceptions.InvalidAuthHeaderException
-    return token
+
+    parts = token.split()
+
+    if parts[0].lower() != "bearer":
+        raise HTTPException(
+            status_code=401,
+            detail='Authorization header must start with Bearer')
+    elif len(parts) == 1:
+        raise HTTPException(
+            status_code=401,
+            detail='Authorization token not found')
+    elif len(parts) > 2:
+        raise HTTPException(
+            status_code=401,
+            detail='Authorization header be Bearer token')
+    auth_token = parts[1]
+    return auth_token
 
 async def get_and_decode_auth_token_from_header(token: str = Header(None)) -> str: #pylint: disable=invalid-name
     """
@@ -23,6 +43,9 @@ async def get_and_decode_auth_token_from_header(token: str = Header(None)) -> st
     and, in the process, decodes the token, returning the payload if valid,
     else raising an authorization exception
     """
+
+
+
     pass
 
 
@@ -31,6 +54,9 @@ async def check_token_valid(token: str) -> bool:
     """
     Placeholder function for checking if a token is decodable (i.e. valid)
     """
+
+    
+
     return len(token) < 5
 
 async def get_payload_from_decoded_token(token: str) -> Dict[str, Any]:
