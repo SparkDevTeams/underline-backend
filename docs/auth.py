725f483397beadd93ba147af3573e3ea4862178e
astherath <farceriv@gmail.com>
2021-03-23 13:09:08 -0400

    Added new header handlers into route and wrote tests for the endpoint.

---
+++ docs/auth.py
@@ -0,0 +1,8 @@
+# pylint: skip-file
+auth_validate_token_desc = """
+Validates authentication token bearing header, returns 400's if
+not present or invalid token, 204 if ok.
+"""
+auth_validate_token_summ = """
+Validate Token
+"""
